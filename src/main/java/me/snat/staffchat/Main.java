db1077dd7e4c38467ce2a538baddcf549333f1ce
MLGPenguin <mlgpenguboi@gmail.com>
2023-01-26 15:38:13 +1000

    *FIX Saving Default Config

--- src/main/java/me/snat/staffchat/Main.java
+++ src/main/java/me/snat/staffchat/Main.java
@@ -3,12 +3,10 @@ package me.snat.staffchat;
 import org.bukkit.plugin.java.JavaPlugin;
 public final class Main extends JavaPlugin {
 
-
-
     @Override
     public void onEnable() {
         getConfig().options().copyDefaults();
-        saveConfig();
+        saveDefaultConfig();
         getCommand("staffchat").setExecutor(new StaffChat(this));
     }
 }
